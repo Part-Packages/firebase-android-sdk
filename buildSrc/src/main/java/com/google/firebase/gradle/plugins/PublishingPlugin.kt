8cfd5aa08d32734236140424a5187b90dbeac4b3
Sina Kadkhodaei <47255141+KadSin@users.noreply.github.com>
2023-07-24 12:12:42 +0330

    Add release.json
    Update RemoteConfigConstants.java
    
    - rename group id
    - add personal maven repository
    
    fix kotlin formatting issue
    
    use foreign server
    
    use local nexus
    
    generate maven artifact zip
    
    update pom generator
    
    Add remote server
    
    change to admin
    
    change maven repo
    
    Rollback extra changes
    
    change nexus user
    
    Separate local zip generator from nexus publisher task
    
    fix java format
    
    Fix nexus publish task
    
    remove extra releasing libraries
    
    add nexus publish releasing libraries task
    
    register nexus task
    
    clean up
    
    Use action secret and variables in `firebasePublish`

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/PublishingPlugin.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/PublishingPlugin.kt
@@ -92,6 +92,8 @@ abstract class PublishingPlugin : Plugin<Project> {
         )
       val publishReleasingLibrariesToBuildDir =
         registerPublishReleasingLibrariesToBuildDirTask(project, releasingProjects)
+      val publishReleasingLibrariesToNexus =
+        registerPublishReleasingLibrariesToNexusTask(project, releasingProjects)
       val generateKotlindocsForRelease =
         registerGenerateKotlindocForReleaseTask(project, releasingFirebaseLibraries)
       val prepareReleaseNotesForDrop =
@@ -105,6 +107,7 @@ abstract class PublishingPlugin : Plugin<Project> {
       registerPublishReleasingLibrariesToMavenLocalTask(project, releasingProjects)
       registerSemverCheckForReleaseTask(project, releasingProjects)
       registerPublishAllToBuildDir(project, allFirebaseLibraries)
+      registerPublishAllToNexus(project, allFirebaseLibraries)
       registerPostReleasePlugin(releasingProjects)
       registerLibraryGroupsTask(project, libraryGroups)
 
@@ -141,6 +144,7 @@ abstract class PublishingPlugin : Plugin<Project> {
           validateLibraryGroupsToPublish,
           checkHeadDependencies,
           // validatePomForRelease, TODO(b/279466888) - Make GmavenHelper testable
+          publishReleasingLibrariesToNexus,
           buildMavenZip,
           buildKotlindocZip,
           buildReleaseNotesZip
@@ -400,6 +404,21 @@ abstract class PublishingPlugin : Plugin<Project> {
       }
     }
 
+  private fun registerPublishReleasingLibrariesToNexusTask(
+    project: Project,
+    releasingProjects: List<Project>
+  ) =
+    project.tasks.register(PUBLISH_RELEASING_LIBS_TO_NEXUS_TASK) {
+      for (releasingProject in releasingProjects) {
+        val publishTask =
+          releasingProject.tasks.named<PublishToMavenRepository>(
+            "publishMavenAarPublicationToNexusRepository"
+          )
+
+        dependsOn(publishTask)
+      }
+    }
+
   /**
    * Registers the [GENERATE_KOTLINDOC_FOR_RELEASE_TASK] task.
    *
@@ -537,6 +556,19 @@ abstract class PublishingPlugin : Plugin<Project> {
       }
     }
 
+  private fun registerPublishAllToNexus(
+    project: Project,
+    allFirebaseLibraries: List<FirebaseLibraryExtension>
+  ) =
+    project.tasks.register(PUBLISH_ALL_TO_NEXUS_TASK) {
+      for (firebaseLibrary in allFirebaseLibraries) {
+        val publishTask =
+          firebaseLibrary.project.tasks.named("publishMavenAarPublicationToNexusRepository")
+
+        dependsOn(publishTask)
+      }
+    }
+
   /** Registers the [PostReleasePlugin] to each releaing project. */
   private fun registerPostReleasePlugin(releasingProjects: List<Project>) {
     for (releasingProject in releasingProjects) {
@@ -558,6 +590,7 @@ abstract class PublishingPlugin : Plugin<Project> {
     const val VALIDATE_POM_TASK = "validatePomForRelease"
     const val LIBRARY_GROUPS_TASK = "libraryGroups"
     const val PUBLISH_RELEASING_LIBS_TO_BUILD_TASK = "publishReleasingLibrariesToBuildDir"
+    const val PUBLISH_RELEASING_LIBS_TO_NEXUS_TASK = "publishReleasingLibrariesToNexus"
     const val PUBLISH_RELEASING_LIBS_TO_LOCAL_TASK = "publishReleasingLibrariesToMavenLocal"
     const val GENERATE_KOTLINDOC_FOR_RELEASE_TASK = "generateKotlindocForRelease"
     const val PREPARE_RELEASE_NOTES_FOR_DROP = "prepareReleaseNotesForDrop"
@@ -568,6 +601,7 @@ abstract class PublishingPlugin : Plugin<Project> {
     const val BUILD_BOM_ZIP_TASK = "buildBomZip"
     const val FIREBASE_PUBLISH_TASK = "firebasePublish"
     const val PUBLISH_ALL_TO_BUILD_TASK = "publishAllToBuildDir"
+    const val PUBLISH_ALL_TO_NEXUS_TASK = "publishAllToNexus"
 
     const val BUILD_DIR_REPOSITORY_DIR = "m2repository"
   }
